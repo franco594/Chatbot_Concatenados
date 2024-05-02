dabd2ef2bada6c1e6407b2a676e813376d6bb927
GastonMarenco <101683116+GastonMarenco@users.noreply.github.com>
2024-05-02 17:56:09 -0300

    Creación del menú
    También se cambiaron los diálogos.

--- Prueba2.py
+++ Prueba2.py
@@ -2,28 +2,38 @@ import tkinter as tk
 from tkinter import scrolledtext
 from nltk.chat.util import Chat, reflections
 
-# Lista de preguntas enumeradas
-preguntas = [
-    "1. ¿Cuál es tu nombre?",
-    "2. ¿Cuál es tu edad?",
-    "3. ¿Dónde vives?",
-    "4. ¿Cuál es tu comida favorita?",
-    "5. ¿Qué haces en tu tiempo libre?"
-]
-
 # Pares de patrones y respuestas para el chatbot
 pares = [
-    ('1', ['Mi nombre es Chatbot.']),
-    ('2', ['Soy un programa de computadora, así que no tengo una edad específica.']),
-    ('3', ['No tengo una ubicación física, existo en la red.']),
-    ('4', ['Como soy un programa, no tengo una comida favorita.']),
-    ('5', ['No tengo tiempo libre, estoy siempre aquí para ayudarte.']),
+    ('hola|ayuda', ['Hola, en que puedo ayudarte?']),
+
+    ('menu|Menu', ['MENU:\n'
+                   '1. Torneos\n'
+                   '2. Horarios\n'
+                   '3. Reglas']),
+
+    ('1', ['Los torneos disponibles son:\n'
+           'bla, bla']),
+
+    ('2', ['Los horarios son:\n'
+           'bla, bla PM']),
+
+    ('3', ['Las reglas son:\n'
+           '1. bla\n'
+           '2. bla']),
+
     ('adiós', ['¡Hasta luego!']),
     ('(.*)', ['Lo siento, no entiendo esa pregunta.']),
 ]
 
 # Crear el chatbot
 chatbot = Chat(pares, reflections)
+
+# Mensaje de bienvenida
+mensaje_bienvenida = ("¡Hola! Soy un Chatbot. ¿En qué puedo ayudarte?\n"
+                      "MENU:\n"
+                      "1. Torneos\n"
+                      "2. Horarios\n"
+                      "3. Reglas")
 
 
 # Función para enviar el mensaje
@@ -38,19 +48,12 @@ def enviar_mensaje():
 # Configuración de la interfaz gráfica
 root = tk.Tk()
 root.title("Chatbot de Consultas con NLTK y Tkinter")
-root.geometry("400x500")
+root.geometry("500x400")
 
 # Área de chat
-chat_area = scrolledtext.ScrolledText(root, width=40, height=20)
+chat_area = scrolledtext.ScrolledText(root, width=58, height=20)
 chat_area.grid(row=0, column=0, padx=10, pady=10, columnspan=2)
-
-# Lista de preguntas
-preguntas_label = tk.Label(root, text="Preguntas:")
-preguntas_label.grid(row=1, column=0, padx=10, pady=10, sticky="w")
-preguntas_listbox = tk.Listbox(root, width=30, height=5)
-for pregunta in preguntas:
-    preguntas_listbox.insert(tk.END, pregunta)
-preguntas_listbox.grid(row=1, column=1, padx=10, pady=10)
+chat_area.insert(tk.END, "Chatbot: " + mensaje_bienvenida + "\n")  # Mostrar mensaje de bienvenida
 
 # Entrada de usuario
 entrada_usuario = tk.Entry(root, width=30)
